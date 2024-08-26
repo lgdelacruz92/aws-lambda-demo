793bd55ec9e1ddb2fc12ac68e39ad2542cf6bf5f
Lester <lgdelacruz92@gmail.com>
2024-08-26 09:50:05 -0400

    configured jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  transform: {
+    "^.+\\.mjs$": "babel-jest",
+  },
+  testRegex: "\\.spec\\.mjs$",
+  moduleFileExtensions: ["js", "mjs"],
+};
---
+++ tests/handler.spec.mjs
@@ -0,0 +1,3 @@
+const { handler } = require("../index.mjs");
+
+test("adds 1 + 2 to equal 3", () => {});
